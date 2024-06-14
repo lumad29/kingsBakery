e44c7da9b6a475b5f2301915bfedc9ef7ca6d9e0
Lucrecia Aloi <lumad29@gmail.com>
2024-06-13 21:50:45 -0300

    second commit

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -3,18 +3,40 @@
 </script>
 
 <template>
-    <div class="home-view">
-        <h1>This is the Home</h1>
-    </div>
+    <v-row class="home-view pa-8">
+        <v-col class="pa-8 ma-8">
+            <div>
+                <h1 class="custom-font-size">We are bread experts </h1>
+                <p class="custom-font-size-2">Enjoy freshly baked bread at our bakery! From hearty whole grain loaves to
+                    fluffy baguettes, we offer
+                    a wide variety to satisfy your cravings. Whether it's a daily loaf or a special treat, we've got you
+                    covered!
+                </p>
+            </div>
+        </v-col>
+        <v-col>
+
+        </v-col>
+    </v-row>
 </template>
 
-<style>
+<style scoped>
 .home-view {
     height: 100vh;
     display: flex;
-    justify-content: center;
+    justify-content: end;
     align-items: center;
     background-color: #37474F;
     color: white;
+}
+
+.custom-font-size {
+    font-size: 3rem;
+    font-family: "Rubik", sans-serif;
+}
+
+.custom-font-size-2 {
+
+    font-family: "Rubik", sans-serif;
 }
 </style>
