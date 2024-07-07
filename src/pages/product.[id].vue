43efab0e3ec3802486e2f95699648808d627137c
Lucrecia Aloi <lumad29@gmail.com>
2024-07-06 22:25:29 -0300

    dynamic product pages

--- src/pages/product.[id].vue
+++ src/pages/product.[id].vue
@@ -4,37 +4,47 @@ const appStore = useAppStore()
 const route = useRoute('/product/[id]') // esto tiene que ver con vue router
 // console.log(appStore.products[0].id);
 
-const productId = computed(() => {
+const selectedProduct = computed(() => {
     // esto tenia que ser convertido a Number
     return appStore.products.find(product => product.id === Number(route.params.id))
 })
 
-const selectedProductItems = computed(() => {
-    return productId.value.items
-})
-
-console.log(productId.value);
-
 </script>
+
 <template>
-    <v-card class="mx-auto" max-width="344" v-for="item in selectedProductItems">
-        <v-img :src="item.img" height="200px" cover />
-        <v-card-title>
-            {{ item.name }}
-        </v-card-title>
+    <div class="background-grey">
+        <v-img :src="selectedProduct.pageImg" height="400" cover>
+            <div class="fill-height d-flex align-center justify-center text-white">
+                <h1>{{ selectedProduct.name }}</h1>
+            </div>
+        </v-img>
+        <v-container class="d-flex">
+            <v-row class="mt-5">
+                <v-col v-for="product in selectedProduct.items" cols="6">
+                    <v-card class="mx-auto bg-grey" width="90%" min-height="500">
+                        <v-img height="400" :src="product.img" cover></v-img>
+                        <v-card-title>
+                            {{ product.name }}
+                        </v-card-title>
 
-        <!-- <v-card-subtitle>
-            {{ item.description }}
-        </v-card-subtitle> -->
+                        <v-card-subtitle>
+                            {{ product.description }}
+                        </v-card-subtitle>
 
-        <v-card-subtitle>
-            {{ item.hello }}
-        </v-card-subtitle>
+                        <v-card-subtitle v-for="ingrediant in product.ingredients">
+                            {{ ingrediant }}
+                        </v-card-subtitle>
 
-        <v-card-actions>
-            <v-btn color="orange-lighten-2" text="Explore"></v-btn>
-            <v-spacer></v-spacer>
 
-        </v-card-actions>
-    </v-card>
+                    </v-card>
+                </v-col>
+            </v-row>
+
+        </v-container>
+    </div>
 </template>
+<style>
+.background-grey {
+    background-color: rgb(55, 71, 79)
+}
+</style>
--- src/store/app.js
+++ src/store/app.js
@@ -1,60 +1,60 @@
-// Utilities
+// Utilities 
 import { defineStore } from 'pinia'
-
 export const useAppStore = defineStore('app', {
   state: () => ({
     products: [
       {
         id: 0,
-        name: 'Breads',
-        items: [{ id: 0, name: 'baggette', price: 2, inStock: true, img: '/assets/products/baguette.jpg', description: 'this is a baguette', hello: 'hello there' },
-        { id: 1, name: 'focaccia', price: 3, inStock: true, img: '/assets/products/focaccia.jpg', description: 'this is a focaccia', hello: 'hello focaccia' }
+        name: 'Our Freshly Made Breads',
+        items: [
+          { id: 0, name: 'Sourdough', price: 2, inStock: true, img: '/assets/sourdough-bread.jpg', description: 'this is sourdough', hello: 'hello sourdough',/*ingredients: ['harina', 'agua']*/ },
+          { id: 1, name: 'Baguette', price: 2, inStock: true, img: '/assets/products/baguette-bread.jpg', description: 'this is a baguette', hello: 'hello baguette',/* ingredients: ['harina', 'agua']*/ },
+          { id: 2, name: 'Brioche', price: 3, inStock: true, img: '/assets/products/brioche-bread.jpg', description: 'this is a Brioche', hello: 'hello Brioche',/* ingredients: ['huevo', 'sal']*/ },
+          { id: 3, name: 'Focaccia', price: 3, inStock: true, img: '/assets/products/focaccia-bread.jpg', description: 'this is a focaccia', hello: 'hello focaccia', /*ingredients: ['huevo', 'sal']*/ },
+          { id: 4, name: 'Sunflower-seed', price: 3, inStock: true, img: '/assets/products/sunflower-seed-bread.jpg', description: 'this is a focaccia', hello: 'hello focaccia', /*ingredients: ['huevo', 'sal']*/ }
+
         ],
+        pageImg: '/assets/flour.jpeg',
         src: '/assets/bread-group.jpg',
         text: `Our bread is baked fresh every morning with the best ingredients. From classic sourdough to unique artisan loaves, each slice is perfect for sandwiches, meals, or just with butter. Enjoy the homemade goodness in every bite.`,
       },
       {
         id: 1,
         name: 'Pastries',
-        items: [{
-          id: 0, name: 'chocolate cupcake', price: 2, inStock: true, img: '/assets/products/chocolate-cupcake.jpg'
-          , description: 'this is a chocolate cupcake'
-        },
-        { id: 1, name: 'vanilla cupcake', price: 1, inStock: false, img: '/assets/products/vanilla-cupcake.jpg', description: 'this is a vanilla cupcake' }
+        items: [
+          { id: 1, name: 'vanilla cupcake', price: 1, inStock: false, img: '/assets/products/vanilla-cupcake.jpg', description: 'this is a vanilla cupcake' },
+          {
+            id: 0, name: 'chocolate cupcake', price: 2, inStock: true, img: '/assets/products/chocolate-cupcake.jpg'
+            , description: 'this is a chocolate cupcake'
+          }
         ],
         src: '/assets/croissant.jpg',
         text: 'Step into our bakery wonderland, where each pastry brings a little piece of joy. Whether you adore flaky croissants, filled danishes, or warm cinnamon rolls, our menu offers something to satisfy every craving'
       },
-      // {
-      //   id: 1,
-      //   name: 'Pastries',
-      //   src: '/assets/croissant.jpg',
-      //   text: 'Step into our bakery wonderland, where each pastry brings a little piece of joy. Whether you adore flaky croissants, filled danishes, or warm cinnamon rolls, our menu offers something to satisfy every craving'
-      // },
-      // {
-      //   id: 2,
-      //   name: 'Cookies',
-      //   src: '/assets/cookies.jpg',
-      //   text: `Welcome to our cozy corner of homemade delights! Our cookies, crafted with love and baked to perfection, offer crunchy edges, gooey centers, and delightful flavors. Whether you love chocolate chip or oatmeal raisin, there's a treat here for you.`
-      // },
-      // {
-      //   id: 3,
-      //   name: 'Cakes and Pies',
-      //   src: '/assets/cake.jpg',
-      //   text: 'Discover our irresistible cakes and pies, each crafted with a blend of expertise and flavor. From rich, velvety cakes to flaky, fruit-packed pies, every slice is a delightful treat for any moment worth celebrating.'
-      // },
-      // {
-      //   id: 4,
-      //   name: 'Gluten-Free & Vegan',
-      //   src: '/assets/gluten-free.jpg',
-      //   text: 'Explore our delicious gluten-free and vegan delights, where flavor meets inclusivity. Dive into a menu that celebrates both health-conscious choices and vibrant flavors, ensuring every bite is a delightful experience for all.'
-      // },
-      // {
-      //   id: 5,
-      //   name: 'Drinks',
-      //   src: '/assets/drinks.jpg',
-      //   text: 'Discover the perfect cup of coffee at our bakery, where we brew each blend to perfection. Our coffee selection is designed to complement our delicious baked goods. We also feature a variety of other refreshing drinks to enjoy. '
-      // }
+      {
+        id: 2,
+        name: 'Cookies',
+        src: '/assets/cookies.jpg',
+        text: `Welcome to our cozy corner of homemade delights! Our cookies, crafted with love and baked to perfection, offer crunchy edges, gooey centers, and delightful flavors. Whether you love chocolate chip or oatmeal raisin, there's a treat here for you.`
+      },
+      {
+        id: 3,
+        name: 'Cakes and Pies',
+        src: '/assets/cake.jpg',
+        text: 'Discover our irresistible cakes and pies, each crafted with a blend of expertise and flavor. From rich, velvety cakes to flaky, fruit-packed pies, every slice is a delightful treat for any moment worth celebrating.'
+      },
+      {
+        id: 4,
+        name: 'Gluten-Free & Vegan',
+        src: '/assets/gluten-free.jpg',
+        text: 'Explore our delicious gluten-free and vegan delights, where flavor meets inclusivity. Dive into a menu that celebrates both health-conscious choices and vibrant flavors, ensuring every bite is a delightful experience for all.'
+      },
+      {
+        id: 5,
+        name: 'Drinks',
+        src: '/assets/drinks.jpg',
+        text: 'Discover the perfect cup of coffee at our bakery, where we brew each blend to perfection. Our coffee selection is designed to complement our delicious baked goods. We also feature a variety of other refreshing drinks to enjoy. '
+      }
     ]
   }),
 })
